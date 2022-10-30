0c635672edaf65a2145c36caf2e328e6633ab9aa
olig8242 <ogrice1991@gmail.com>
2022-10-30 20:39:10 +0000

    added JS

---
+++ script-el.js
@@ -0,0 +1,89 @@
+let currentNumber;
+let equalsNumber;
+let target;
+
+
+calc1.addEventListener("click", () => {
+    calcScreen.textContent += "1"
+})
+
+calc2.addEventListener("click", () => {
+    calcScreen.textContent += "2"
+})
+
+calc3.addEventListener("click", () => {
+    calcScreen.textContent += "3"
+})
+
+calc4.addEventListener("click", () => {
+    calcScreen.textContent += "4"
+})
+
+calc5.addEventListener("click", () => {
+    calcScreen.textContent += "5"
+})
+
+calc6.addEventListener("click", () => {
+    calcScreen.textContent += "6"
+})
+
+calc7.addEventListener("click", () => {
+    if (plus == true && currentNumber !== "") {
+        calcScreen.textContent = "7"
+        plus = false;
+    }
+    else {
+        calcScreen.textContent += "9" 
+    }
+})
+
+calc8.addEventListener("click", () => {
+    if (plus == true && currentNumber !== "") {
+        calcScreen.textContent = "8"
+        plus = false;
+    }
+    else {
+        calcScreen.textContent += "8" 
+    }
+})
+calc9.addEventListener("click", () => {
+    if (plus == true && currentNumber !== "") {
+        calcScreen.textContent = "9"
+        plus = false;
+    }
+    else {
+        calcScreen.textContent += "9" 
+    }
+})
+
+calcPlus.addEventListener("click", () => {
+    target = "plus"
+    plus = true;
+    minus = false;
+    if (calcScreen.textContent !== "") {
+        currentNumber = calcScreen.textContent;
+        console.log(currentNumber);
+    }
+}) 
+
+calcSubtract.addEventListener("click", () => {
+    target = "subtract";
+    plus = false;
+    minus = true;
+    
+})
+
+
+calcEquals.addEventListener("click", () => {
+    if (target == "plus") {
+    equalsNumber = calcScreen.textContent = parseInt(currentNumber) + parseInt(calcScreen.textContent);
+    }
+})
+
+
+
+//plus 
+//take current number, 
+//change plus to true
+//next number added needs to start to overwrite current number
+
---
+++ script.js
@@ -0,0 +1,28 @@
+//plan
+
+//create calculator design
+//have buttons work so screen changes
+//ensure buttons can interact with each other
+let plus = false;
+let minus = false;
+let divide = false;
+let multiply = false;
+
+//variables
+const calcScreen = document.querySelector(".calc-screen");
+
+const calc1 = document.querySelector(".calc-1");
+const calc2 = document.querySelector(".calc-2");
+const calc3 = document.querySelector(".calc-3");
+
+const calc4 = document.querySelector(".calc-4");
+const calc5 = document.querySelector(".calc-5");
+const calc6 = document.querySelector(".calc-6");
+
+const calc7 = document.querySelector(".calc-7");
+const calc8 = document.querySelector(".calc-8");
+const calc9 = document.querySelector(".calc-9");
+
+const calcSubtract = document.querySelector(".calc-subtract");
+const calcPlus = document.querySelector(".calc-plus");
+const calcEquals = document.querySelector(".calc-equals");
